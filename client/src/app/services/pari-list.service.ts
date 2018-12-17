b1722fe60c59ceb0d7d00098687d4bfbbd0a5062
Nick Filatov <nfilatov@foundationmedicine.com>
2018-12-17 15:57:07 -0500

    httpClient added

--- client/src/app/services/pari-list.service.ts
+++ client/src/app/services/pari-list.service.ts
@@ -1,12 +1,26 @@
 import { Pari} from '../shared/pari';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
-export class PariListService{
+export class PariListService implements OnInit{
     pariList: Pari[];
-    getPariList();
+
+    constructor(private http: HttpClient) {
+        
+    }
+
+    ngOnInit() {
+        this.getPariList();
+    }
 
     getPariList() : Pari[]{
+        var s = this.http.get("https://ctd84g68jc.execute-api.us-east-1.amazonaws.com/prod/pari").subscribe(value => {
+            console.log(value);
+        }, error => { 
+            console.log(error)
+        });
+
         this.pariList = [
             {name: "Выебать Лёху на тему Курил", id: 'a532852b-cab6-4db8-94c7-db447ff65c8e', date: new Date(2019, 1, 1), judges: ['Вася', 'Петя'], attitudes: null}, 
             {name: "Поиметь Бобруцкова на тему Кокорина и Мамаева", id: '086d2f11-775c-425d-9bb4-c2ce107c0cc7', date: new Date(2018, 12, 5), judges: ['Пьерлуиджи Колина'], attitudes: null}
--- server/PariManager/Function.cs
+++ server/PariManager/Function.cs
@@ -7,9 +7,8 @@ namespace PariManager
 {
     public class Function
     {
-        
         /// <summary>
-        /// A simple function that takes a string and does a ToUpper
+        /// 
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
